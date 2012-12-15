db556ce0ef7c13b75afc70714c0ecc3e303c4a44
Chris Continanza <christopher.continanza@gmail.com>
2012-12-15 13:37:26 -0800

    ready for prod

--- app.js
+++ app.js
@@ -25,10 +25,12 @@ var lists = [];
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
   process.env.PORT = process.env.PORT || 3333;
+  checkcheckit_url = "http://192.168.1.144:" + process.env.PORT + '/'
 });
 
 app.configure('production', function(){
   app.use(express.errorHandler());
+  checkcheckit_url = 'http://checkcheckit.herokuapp.com/'
 });
 
 app.get('/:list_id', function(req, res){
@@ -55,11 +57,11 @@ app.post('/', function(req, res){
   lists[i] = list
 
   var subject = "Start list " + list.name;
-  var message = "http://192.168.1.144:" + process.env.PORT + "/" + i
+  var message = checkcheckit_url + '' + i
   mailer.send(emails, subject, message)
 
   res.send(200, i)
 })
 
 app.listen(process.env.PORT);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+console.log("Express server listening on port %d in %s mode", process.env.PORT, app.settings.env);
