8a22698db27e36e56793013c6642344ba7dbda99
Chris Continanza <christopher.continanza@gmail.com>
2012-12-20 11:35:20 -0800

    tested listParser::parse

--- list.js
+++ list.js
@@ -1,22 +1,35 @@
 module.exports = {}
 
 module.exports.parse = function(string){
-  var steps = []
-  var lines = string.split("\n").reverse()
-  var current_step = null;
+  var lines = string.split("\n")
+  var list = {};
 
+  var first_line = lines[0].trim();
+  if(first_line[0] == '#'){
+    list.description = first_line.replace(/^\#/,'').trim()
+  }
+
+  lines = lines.reverse();
+  var steps = []
+  var current_step = null;
   while(lines.length > 0){
     var line = lines.pop();
     if( line[0] == '-'){
+      //create a new step
       current_step = {
-        'title' : line.replace(/^-/,'').trim()
+        'title' : line.replace(/^-/,'').trim(),
+        'body'  : ''
       }
       steps.push(current_step)
     }
+    else if(current_step){
+      // add the line to the body
+      if( current_step.body != ''){ line = '\n' + line }
+      current_step.body = current_step.body + line
+    }
   }
 
-  return {
-    'steps' : steps
-  }
+  list.steps = steps
 
+  return list
 }
--- test/test.js
+++ test/test.js
@@ -12,7 +12,32 @@ describe('List', function(){
     })
 
     it('should assume everything beneath a step is the body', function(){
+      var list = listParser.parse("- apples\n tasty treats\n good to eat\n- bananas")
+      assert.equal('apples',  list.steps[0].title);
+      assert.equal(' tasty treats\n good to eat',  list.steps[0].body);
+      assert.equal('bananas', list.steps[1].title);
+    })
 
+    it('should assume the first line is a description if it begins with #', function(){
+      var list = listParser.parse("# Groceries \n- apples\n tasty treats\n good to eat\n- bananas")
+      assert.equal('Groceries',  list.description);
+      assert.equal('apples',  list.steps[0].title);
+      assert.equal('bananas', list.steps[1].title);
+    })
+
+    it('should handle complicated lists', function(){
+      var list_string = '# My List \n'
+      list_string += '- do a thing\n make it good\n  > `run this command`\n'
+      list_string += '- again!\n'
+      list_string += '- now you\'ve got it! \n yay!'
+
+      var list = listParser.parse(list_string);
+      assert.equal('My List',    list.description);
+      assert.equal('do a thing', list.steps[0].title);
+      assert.equal(' make it good\n  > `run this command`', list.steps[0].body);
+      assert.equal('again!', list.steps[1].title);
+      assert.equal("now you've got it!", list.steps[2].title);
+      assert.equal(" yay!", list.steps[2].body);
     })
   })
 })
